9686da6861cff91572b5f70a8e849bcbc42867dc
eccenux <eccenux+git@gmail.com>
2023-07-11 17:21:59 +0200

    load full history (page by page)

--- load.js
+++ load.js
@@ -5,28 +5,60 @@ import fetch from 'node-fetch';
  */
 class LoadData {
 	constructor(url) {
+		/** Initial URL. */
 		this.url = url;
+		/** Page history. */
+		this.history = [];
 	}
 
+	/**
+	 * Load history entries.
+	 */
 	async load() {
-		const response = await fetch(this.url);
-		this.loadPage(response);
+		let url = this.url;
+		do {
+			const response = await fetch(url);
+			url = await this.loadPage(response);
+		} while (url)
 	}
 
+	/** History info. */
+	info() {
+		console.log('Data loaded:', this.history.length);
+		const first = this.history[this.history.length - 1];
+		const last = this.history[0];
+		console.log('First edit:', first);
+		console.log('Last edit:', last);
+	}
+
+	/**
+	 * Load page.
+	 * @private
+	 * 
+	 * Typically the page would have 20 entries.
+	 * 
+	 * @param {Response} response Page result.
+	 * @returns Next URL.
+	 */
 	async loadPage(response) {
 		const data = await response.json();
 
 		console.log(data.revisions.length, data.older);
 		for (const revision of data.revisions) {
-			console.log({
+			const entry = {
 				dt: revision.timestamp,
 				message: revision.comment,
 				author: revision.user.name
-			})
+			};
+			this.history.push(entry);
+			console.log(entry);
 		}
+
+		return data.older;
 	}
 }
 
 const url = 'https://en.wikipedia.org/w/rest.php/v1/page/Agapanthus/history';
 const loader = new LoadData(url);
-loader.load();
+await loader.load();
+loader.info();
