940768836b9e253d2844d29401a2ec8efcae22aa
eccenux <eccenux+git@gmail.com>
2023-07-11 19:58:41 +0200

    poc: load a revision content * separate site name * build url * download first and last revision

--- load.js
+++ load.js
@@ -1,12 +1,19 @@
 import fetch from 'node-fetch';
+import fs from 'fs';
+import fsa from 'fs/promises';
 
 /**
  * Download page history from a Mediawiki site.
  */
 class LoadData {
-	constructor(url) {
+	/**
+	 * Init.
+	 * @param {String} site Domain of the Wiki. 
+	 * @param {String} page Page/Article (with namespace).
+	 */
+	constructor(site, page) {
 		/** Initial URL. */
-		this.url = url;
+		this.url = `https://${site}/w/rest.php/v1/page/${page}/history`;		;
 		/** Page history. */
 		this.history = [];
 	}
@@ -20,6 +27,7 @@ class LoadData {
 			const response = await fetch(url);
 			url = await this.loadPage(response);
 		} while (url)
+		await this.poc();
 	}
 
 	/** History info. */
@@ -37,6 +45,9 @@ class LoadData {
 	 * 
 	 * Typically the page would have 20 entries.
 	 * 
+	 * Example start url:
+	 * https://en.wikipedia.org/w/rest.php/v1/page/Agapanthus/history
+	 * 
 	 * @param {Response} response Page result.
 	 * @returns Next URL.
 	 */
@@ -47,8 +58,9 @@ class LoadData {
 		for (const revision of data.revisions) {
 			const entry = {
 				dt: revision.timestamp,
+				author: revision.user.name,
+				id: revision.id,
 				message: revision.comment,
-				author: revision.user.name
 			};
 			this.history.push(entry);
 			console.log(entry);
@@ -56,9 +68,66 @@ class LoadData {
 
 		return data.older;
 	}
+
+	/**
+	 * Load full revision data.
+	 * @private
+	 * 
+	 * Example url:
+	 * https://en.wikipedia.org/w/rest.php/v1/revision/764138197
+	 * 
+	 * @param {Number} id Revision ID.
+	 * @returns Revision data.
+	 */
+	async loadRev(id) {
+		let url = `https://${site}/w/rest.php/v1/revision/${id}`;
+		const response = await fetch(url);
+		const data = await response.json();
+
+		console.log(data.timestamp, data.page.title, data.user.name);
+
+		return data.source;
+	}
+
+	/**
+	 * Load and save revision as a file.
+	 * @private
+	 * 
+	 * @param {Number} id Revision ID.
+	 * @param {String} dstFile Destionation path.
+	 * @returns Revision data.
+	 */
+	async saveRev(id, dstFile) {
+		// const data = await fsa.readFile(srcJs, 'utf8');
+		const data = await this.loadRev(id);
+		await fsa.writeFile(dstFile, data);
+		return data;
+	}
+
+	/**
+	 * PoC: load content.
+	 * 
+	 * TODO: Later probably load rev, save and commit.
+	 * Maybe do separately from loading history 
+	 * to allow creating history JSON in different ways later...
+	 * Someone might want to merge history of two pages I guess...
+	 */
+	async poc() {
+		const first = this.history[this.history.length - 1];
+		const last = this.history[0];
+		const dir = 'repo/';
+		if (!fs.existsSync(dir)){
+			fs.mkdirSync(dir);
+		}
+		await Promise.all([
+			this.saveRev(first.id, dir + '1st.tex'),
+			this.saveRev(last.id, dir + 'last.tex'),
+		]);
+	}
 }
 
-const url = 'https://en.wikipedia.org/w/rest.php/v1/page/Agapanthus/history';
-const loader = new LoadData(url);
+const site = 'en.wikipedia.org';
+const page = 'Agapanthus';
+const loader = new LoadData(site, page);
 await loader.load();
 loader.info();
